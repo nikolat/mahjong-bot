e89d11cbbd9a2b0f69d87bbd68a4df185c93d8c3
Don <evidence@384.jp>
2023-11-08 18:04:02 +0900

    カスタム絵文字で表示

--- src/response.ts
+++ src/response.ts
@@ -210,10 +210,13 @@ const res_join = (event: NostrEvent): [string, string[][]][] | null => {
 		for (let i = 0; i <= 3; i++) {
 			tehai[i] = yama.slice(0 + 13*i, 13 + 13*i);
 			tehai[i].sort(compareFn);
-			res.push([`nostr:${nip19.npubEncode(players[i])} haipai ${tehai[i].join('')}`, [...getTagsAirrep(event), ['p', players[i], '']]]);
+			const content = `nostr:${nip19.npubEncode(players[i])} haipai\n${tehai[i].map(pi => `:${convertEmoji(pi)}:`).join('')}`;
+			const emoijTags = Array.from(new Set(tehai[i])).map(pi => ['emoji', convertEmoji(pi), getEmojiUrl(pi)]);
+			const tags = [...getTagsAirrep(event), ['p', players[i], ''], ...emoijTags];
+			res.push([content, tags]);
 		}
 		const tsumo = yama[4*13+1];
-		res.push([`nostr:${nip19.npubEncode(players[0])} tsumo ${tsumo}`, [...getTagsAirrep(event), ['p', players[0], '']]]);
+		res.push([`nostr:${nip19.npubEncode(players[0])} tsumo\n:${convertEmoji(tsumo)}:`, [...getTagsAirrep(event), ['p', players[0], ''], ['emoji', convertEmoji(tsumo), getEmojiUrl(tsumo)]]]);
 		return res;
 	}
 	return null;
@@ -247,4 +250,74 @@ const compareFn = (a: string, b: string) => {
 		return 1;
 	}
 	return 0;
+};
+
+const convertEmoji = (pai: string) => {
+	if (['m', 'p', 's'].includes(pai.at(1) ?? '')) {
+		return `mahjong_${pai.at(1)}${pai.at(0)}`;
+	}
+	else if (pai.at(1) === 'z') {
+		switch (pai.at(0)) {
+			case '1':
+				return 'mahjong_east';
+			case '2':
+				return 'mahjong_south';
+			case '3':
+				return 'mahjong_west';
+			case '4':
+				return 'mahjong_north';
+			case '5':
+				return 'mahjong_white';
+			case '6':
+				return 'mahjong_green';
+			case '7':
+				return 'mahjong_red';
+			default:
+				throw TypeError(`Unknown pai: ${pai}`);
+		}
+	}
+	else {
+		throw TypeError(`Unknown pai: ${pai}`);
+	}
+};
+
+const getEmojiUrl = (pai: string): string => {
+	return awayuki_mahjong_emojis[convertEmoji(pai)];
+};
+
+const awayuki_mahjong_emojis: any = {
+	"mahjong_m1": "https://awayuki.github.io/emoji/mahjong-m1.png",
+	"mahjong_m2": "https://awayuki.github.io/emoji/mahjong-m2.png",
+	"mahjong_m3": "https://awayuki.github.io/emoji/mahjong-m3.png",
+	"mahjong_m4": "https://awayuki.github.io/emoji/mahjong-m4.png",
+	"mahjong_m5": "https://awayuki.github.io/emoji/mahjong-m5.png",
+	"mahjong_m6": "https://awayuki.github.io/emoji/mahjong-m6.png",
+	"mahjong_m7": "https://awayuki.github.io/emoji/mahjong-m7.png",
+	"mahjong_m8": "https://awayuki.github.io/emoji/mahjong-m8.png",
+	"mahjong_m9": "https://awayuki.github.io/emoji/mahjong-m9.png",
+	"mahjong_p1": "https://awayuki.github.io/emoji/mahjong-p1.png",
+	"mahjong_p2": "https://awayuki.github.io/emoji/mahjong-p2.png",
+	"mahjong_p3": "https://awayuki.github.io/emoji/mahjong-p3.png",
+	"mahjong_p4": "https://awayuki.github.io/emoji/mahjong-p4.png",
+	"mahjong_p5": "https://awayuki.github.io/emoji/mahjong-p5.png",
+	"mahjong_p6": "https://awayuki.github.io/emoji/mahjong-p6.png",
+	"mahjong_p7": "https://awayuki.github.io/emoji/mahjong-p7.png",
+	"mahjong_p8": "https://awayuki.github.io/emoji/mahjong-p8.png",
+	"mahjong_p9": "https://awayuki.github.io/emoji/mahjong-p9.png",
+	"mahjong_s1": "https://awayuki.github.io/emoji/mahjong-s1.png",
+	"mahjong_s2": "https://awayuki.github.io/emoji/mahjong-s2.png",
+	"mahjong_s3": "https://awayuki.github.io/emoji/mahjong-s3.png",
+	"mahjong_s4": "https://awayuki.github.io/emoji/mahjong-s4.png",
+	"mahjong_s5": "https://awayuki.github.io/emoji/mahjong-s5.png",
+	"mahjong_s6": "https://awayuki.github.io/emoji/mahjong-s6.png",
+	"mahjong_s7": "https://awayuki.github.io/emoji/mahjong-s7.png",
+	"mahjong_s8": "https://awayuki.github.io/emoji/mahjong-s8.png",
+	"mahjong_s9": "https://awayuki.github.io/emoji/mahjong-s9.png",
+	"mahjong_east": "https://awayuki.github.io/emoji/mahjong-east.png",
+	"mahjong_south": "https://awayuki.github.io/emoji/mahjong-south.png",
+	"mahjong_west": "https://awayuki.github.io/emoji/mahjong-west.png",
+	"mahjong_north": "https://awayuki.github.io/emoji/mahjong-north.png",
+	"mahjong_white": "https://awayuki.github.io/emoji/mahjong-white.png",
+	"mahjong_green": "https://awayuki.github.io/emoji/mahjong-green.png",
+	"mahjong_red": "https://awayuki.github.io/emoji/mahjong-red.png",
 };
