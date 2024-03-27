51f4ef5defd47c035f5576992061bb5c808821f1
Don <496156+nikolat@users.noreply.github.com>
2024-03-28 07:35:56 +0900

    access log

--- src/page.ts
+++ src/page.ts
@@ -3,7 +3,10 @@ import express from 'express';
 export const page = () => {
 	const app = express();
 	const port = process.env.PORT || 3001;
-	app.get('/', (req, res) => res.status(204).send(''));
+	app.get('/', (req, res) => { 
+		console.log('root access', req);
+		res.status(204).send('');
+	});
 	const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 	server.keepAliveTimeout = 120 * 1000;
 	server.headersTimeout = 120 * 1000;
