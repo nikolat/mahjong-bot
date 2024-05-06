1e68f4742f81745fa64dfde720c29f25ce44c004
Don <496156+nikolat@users.noreply.github.com>
2024-05-06 15:58:44 +0900

    fix

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export const getNsecs = (): (string | undefined)[] => {
 	const nsec_whanwhan = process.env.NOSTR_PRIVATE_KEY_WHANWHAN;
 	const nsec_bee = process.env.NOSTR_PRIVATE_KEY_BEE;
 	const nsec_unyu = process.env.NOSTR_PRIVATE_KEY_UNYU;
-	return [nsec_jongbari, nsec_rinrin, nsec_chunchun, nsec_whanwhan, nsec_unyu];
+	return [nsec_jongbari, nsec_rinrin, nsec_chunchun, nsec_whanwhan, nsec_bee, nsec_unyu];
 };
 
 export const isDebug = false;
