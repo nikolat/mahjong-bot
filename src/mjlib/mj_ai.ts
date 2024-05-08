9463732aac705cfb40a0fa0575389be661ad4d2f
Don <496156+nikolat@users.noreply.github.com>
2024-05-08 21:24:46 +0900

    fix

--- src/mjlib/mj_ai.ts
+++ src/mjlib/mj_ai.ts
@@ -14,7 +14,7 @@ export const naniwokiru = (
 	aryPlayerGenbutsu: string[],
 	strVisiblePai: string,
 ): string => {
-	const arVisiblePai = stringToArrayWithFuro(strVisiblePai)[0];
+	const arVisiblePai: string = stringToArrayWithFuro(strVisiblePai)[0].join('');
 	const arVisibleNum = [
 		0, 0, 0, 0, 0, 0, 0, 0, 0,
 		0, 0, 0, 0, 0, 0, 0, 0, 0,
