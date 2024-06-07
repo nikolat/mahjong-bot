3d798ac88b420cf8437ff7033e32e3bb18c8d0df
Don <496156+nikolat@users.noreply.github.com>
2024-06-07 19:02:17 +0900

    revert NIP-25

--- src/main.ts
+++ src/main.ts
@@ -116,6 +116,7 @@ const main = async () => {
 			);
 			const getTagsFav = (event: NostrEvent): string[][] => {
 				const tagsFav: string[][] = [
+					...event.tags.filter(tag => tag.length >= 2 && (tag[0] === 'e' || (tag[0] === 'p' && tag[1] !== event.pubkey))),
 					['e', event.id, '', ''],
 					['p', event.pubkey, ''],
 					['k', String(event.kind)]
